b5c659aa5b16b109c66388b4ad671299f41c2d33
khurram jaffery <khurramjaffery12@gmail.com>
2023-11-05 14:59:27 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  var swiper = new Swiper(".heySwiper", {
+    slidesPerView: 2,
+    spaceBetween:10,
+    
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+  });
